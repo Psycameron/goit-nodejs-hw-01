c815e8ea91bae95b71c8da7f4ecb9330888b20a2
Dmitriy Bulakhov <bulakhovda@gmail.com>
2023-04-03 18:20:33 +0300

    Added 3 functions

---
+++ db/contacts.js
@@ -0,0 +1,43 @@
+const { nanoid } = require("nanoid");
+
+const fs = require("fs/promises");
+const path = require("path");
+
+const contactsPath = path.join(__dirname, "contacts.json");
+
+async function updateList(contacts) {
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+}
+
+async function listContacts() {
+  const data = await fs.readFile(contactsPath);
+  return JSON.parse(data);
+}
+
+async function getContactById(contactId) {
+  const allContacts = await listContacts();
+  const contactByID = allContacts.find((contact) => contact.id === contactId);
+  return contactByID || null;
+}
+
+async function addContact({ name, email, phone }) {
+  const allContacts = await listContacts();
+  const newContacts = {
+    id: nanoid(),
+    name,
+    email,
+    phone,
+  };
+  contacts = [...allContacts, newContacts];
+  await updateList(contacts);
+  return contacts;
+}
+
+async function removeContact(contactId) {}
+
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+};
---
+++ index.js
@@ -0,0 +1,36 @@
+const argv = require("yargs").argv;
+
+const {
+  listContacts,
+  getContactById,
+  addContact,
+  removeContact,
+} = require("./db/contacts");
+
+async function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case "list":
+      const allContacts = await listContacts();
+      console.log(`🚀 ~ invokeAction ~ allContacts:`, allContacts);
+      break;
+
+    case "get":
+      const contactByID = await getContactById(id);
+      console.log(`🚀 ~ invokeAction ~ contactByID:`, contactByID);
+      break;
+
+    case "add":
+      const newContactsList = await addContact({ name, email, phone });
+      console.log(`🚀 ~ invokeAction ~ newContactsList:`, newContactsList);
+      break;
+
+    case "remove":
+      // ... id
+      break;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+}
+
+invokeAction(argv);
